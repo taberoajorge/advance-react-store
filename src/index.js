24018b75696106e3d547897834825ce77f8e817e
Jorge Alexander Taberoa Jimenez <taberoajorge@pm.me>
2022-01-13 09:59:18 -0600

    finish develpment enviroment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './routes/App';
+
+ReactDom.render(
+  <App />,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/inline',
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
